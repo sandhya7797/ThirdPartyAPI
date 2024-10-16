562d40f2302b941eb4bc84ec73852d055e37fccc
Sandhya <ragulapadusandhya7797@gmail.com>
2024-10-16 14:42:11 -0700

    first commit

---
+++ src/main/java/com/scaler/thirdpartyapi/ThirdPartyApiApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.thirdpartyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPartyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdPartyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/thirdpartyapi/ThirdPartyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.thirdpartyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdPartyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
