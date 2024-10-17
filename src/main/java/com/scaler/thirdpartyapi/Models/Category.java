2d9a5150c55b921e8e92ceb25c5ac0f8ac228100
Sandhya <ragulapadusandhya7797@gmail.com>
2024-10-17 15:59:09 -0700

    Database Setup added

--- src/main/java/com/scaler/thirdpartyapi/Models/Category.java
+++ src/main/java/com/scaler/thirdpartyapi/Models/Category.java
@@ -1,12 +1,15 @@
 package com.scaler.thirdpartyapi.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category {
-
+    @Id
     private long id;
     private String name;
 
--- src/main/java/com/scaler/thirdpartyapi/Models/Product.java
+++ src/main/java/com/scaler/thirdpartyapi/Models/Product.java
@@ -1,16 +1,21 @@
 package com.scaler.thirdpartyapi.Models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
-
+    @Id
     private long id;
     private String title;
     private double price;
+    @ManyToOne
     private Category category;//made it separate class bec if we want to test new category we can simply add it to database and test without restarting the application.
     private String description;
     private String image;
